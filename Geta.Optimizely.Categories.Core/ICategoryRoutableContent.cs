127d852ee25858eb592a3f652402ea3391c86b52
Ivan Ivanov <IIvanov@indeavr.com>
2022-11-23 19:25:46 +0200

    created core project

---
+++ Geta.Optimizely.Categories.Core/ICategoryRoutableContent.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Geta Digital. All rights reserved.
+// Licensed under Apache-2.0. See the LICENSE file in the project root for more information
+
+using EPiServer.Core;
+
+namespace Geta.Optimizely.Categories.Core
+{
+    public interface ICategoryRoutableContent : IContent
+    {
+    }
+}
---
+++ Geta.Optimizely.Categories.Core/Routing/CategoryModelBinder.cs
@@ -0,0 +1,49 @@
+// Copyright (c) Geta Digital. All rights reserved.
+// Licensed under Apache-2.0. See the LICENSE file in the project root for more information
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EPiServer.Core;
+using EPiServer.Globalization;
+using EPiServer.Web.Routing;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Geta.Optimizely.Categories.Core.Routing
+{
+    public class CategoryModelBinder : IModelBinder
+    {
+        private readonly ICategoryContentLoader _categoryContentLoader;
+        private readonly IPageRouteHelper _pageRouteHelper;
+
+        public CategoryModelBinder(
+            ICategoryContentLoader categoryContentLoader,
+            IPageRouteHelper pageRouteHelper)
+        {
+            _categoryContentLoader = categoryContentLoader;
+            _pageRouteHelper = pageRouteHelper;
+        }
+
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            var categories = GetCategoriesFromRequest(bindingContext.HttpContext.Request);
+            bindingContext.Result = ModelBindingResult.Success(categories);
+            return Task.CompletedTask;
+        }
+
+        private IList<CategoryData> GetCategoriesFromRequest(HttpRequest request)
+        {
+            var categorySegments = (string[])request.RouteValues[CategoryRoutingConstants.CurrentCategories];
+            if (categorySegments == null)
+            {
+                return new List<CategoryData>();
+            }
+            
+            var culture = (_pageRouteHelper.Content as ILocale).Language ?? ContentLanguage.PreferredCulture;
+
+            return categorySegments.Select(x => _categoryContentLoader.GetFirstBySegment<CategoryData>(x, culture))
+                                   .ToList();
+        }
+    }
+}
