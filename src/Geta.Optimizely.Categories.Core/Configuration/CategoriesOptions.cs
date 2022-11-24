44701fe581e51eb7918f23892fa6b4449c324a34
Ivan Ivanov <IIvanov@indeavr.com>
2022-11-24 09:30:11 +0200

    Added core files to Core project

---
+++ src/Geta.Optimizely.Categories.Core/Configuration/CategoriesOptions.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Geta Digital. All rights reserved.
+// Licensed under Apache-2.0. See the LICENSE file in the project root for more information
+
+namespace Geta.Optimizely.Categories.Core.Configuration
+{
+    public class CategoriesOptions
+    {
+        public string CategorySeparator { get; set; } = "__";
+        public bool DisableCategoryAsLinkableType { get; set; }
+        public bool HideDisallowedRootCategories { get; set; }
+        public bool ShowDefaultCategoryProperty { get; set; }
+    }
+}
